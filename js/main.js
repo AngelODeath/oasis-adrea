6b0d40bff3483450d047110ab46eea92866c7f86
Gabri Botha <gabri@principalsoftware.co.za>
2014-11-15 14:04:12 +0200

    Moved stratus player code to main.js. Start using turnjs. - Add turnjs initalize code to main.js. Remove page_right style (not use anymore) Cj's article added to page 2.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function() {
+    $.stratus({
+        links: 'https://soundcloud.com/foofighters/sets/wasting-light'
+    });
+    
+    $("#flipbook").turn({
+        width: 1500,
+        height: 1060.5,
+        autoCenter: true
+    });
+    
+});
