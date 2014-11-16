6b39322fbad5fe3918fced9f73deb4f41bd386b2
Gabri Botha <gabri@principalsoftware.co.za>
2014-11-16 13:26:01 +0200

    ad js and css for flip

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.stratus({
         links: '//soundcloud.com/room1011/aphex-twins-pacman-powerpill'
     });
-    
+
     $("#flipbook").turn({
         width: 1000,
         height: 707.5,
@@ -12,10 +12,17 @@ $(document).ready(function() {
         acceleration: true,
         display: "double"
     });
-    
+
     $('[data-toggle="tooltip"]').tooltip();
-    
+
     $('#culture_article_1').click(function() {
+        $('#flipbook').turn('page', '3');
     });
-    
+
+    $('.flip-container').bind('touchstart', function() {
+        $(this).addClass('hover');
+    }).bind('touchend', function() {
+        $(this).removeClass('hover');
+    });
+
 });
